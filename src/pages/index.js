a374990bb84b61eb1d38f4e75b5cf12a80ed242d
VALERIE PIETRI <Emma@macbook-pro-de-emma.home>
2021-01-31 11:11:03 +0100

    pour voir difference mac book pro and mine

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "/Users/Emma/hello-world/src/pages/components/header"
+import Layout from "/Users/Emma/hello-world/src/pages/components/layout"
+
 
 export default function Home() {
-  return <div>What did you accomplish today?</div>
+
+  return (
+    <form>
+    <button type="submit" class="button2" >Submit</button>
+ 
+
+    <div style={{ margin: `2rem auto`, maxWidth: 600 , color: `purple` }}>
+    <Link to="/contact">Contact</Link>
+    <h1>What have you accomplished today?</h1>
+    <p>What a world. </p>
+    <Layout>
+   {
+    <img src="https://images.unsplash.com/photo-1540991825428-5b54b09f7338?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1197&q=80" alt="" class="dogpicture" />
+  }
+    </Layout>
+  </div>
+  </form>
+  );
 }
