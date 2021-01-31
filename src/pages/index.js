5f069417f7122e3f48c40f42d505343d5a568c77
Stan Kalfon <stankalfon@gmail.com>
2021-01-31 11:36:50 +0100

    Mario commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Greg is bae</div>
+  return <div>
+    <style>
+    
+  
+    </style>
+    It's me Mario
+    <div>This is a test </div>
+    
+    <div> type button 1 </div>
+    <a href="http://google.com">
+    <input type="submit" value = "hey you" class />
+    </a>
+
+  </div>
+        
+
+
+
+
+
+  
+
 }
